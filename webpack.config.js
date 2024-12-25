74b24d9d316c2e960a64a55137e47f219ba48984
dhas359 <rilaroko1973@gmail.com>
2024-12-25 20:27:14 +0500

    Build project and update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,32 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.js', // путь к вашему входному файлу
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // имя выходного файла
+    path: path.resolve(__dirname, 'dist'), // папка для собранных файлов
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
+    static: path.resolve(__dirname, 'dist'), // статический контент
+    compress: true, // сжимаем файлы
+    port: 9000, // порт для разработки
   },
   module: {
     rules: [
       {
-        test: /\.css$/, // Правило для CSS
+        test: /\.css$/, // правило для CSS
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.mp4$/,
+        test: /\.mp4$/, // правило для MP4 файлов
         use: 'file-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html', // шаблон для генерации HTML
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(), // чистка папки dist перед каждой сборкой
   ],
 };
